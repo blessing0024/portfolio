848d3576f7d362615b8ba0d9225477e83838cae2
Onome Blessing <onomeblessing4002@gmail.com>
2023-07-12 19:42:34 +0100

    continued working further on my portfolio

--- script.js
+++ script.js
@@ -3,5 +3,16 @@
 let MenuBtn = document.getElementById('MenuBtn');
 
 MenuBtn.addEventListener('click', function(e) {
-    
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
 })
+
+
+let typed = new Typed(".auto-input", {
+    strings: ['Young-Tech-Lady', 'Website Developer', 'Full Stack Developer', 'Brand Expert', 'Tech Enthusiast', ],
+
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+} )
